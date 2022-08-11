e1abfb4803b2b8b935cb23eb9d0c32bb22604a96
Brayan <becg852456@gmail.com>
2022-08-10 22:37:23 -0500

    Datos iniciales del proyecto de interes del ViewBinding

---
+++ app/src/main/java/com/black/viewbinding/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.black.viewbinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.black.viewbinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvMessage.text = "Contenido nuevo"
+    }
+}
